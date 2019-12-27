ec186594d92a25e76fa460c53d398cbed52b06ca
azessai <azeddine.essai@gmail.com>
2019-12-27 16:33:05 +0100

    class valeur par defaut

---
+++ tp_class_eleve.py
@@ -0,0 +1,10 @@
+class Eleve:
+    def __init__(self, nom, age, taille=150):
+        self.nom = nom
+        self.age = age
+        self.taille = taille
+        print("Nouveau joueur : ", self.nom, 'agé de', self.age, 'ans et mesure', self.taille, 'cm')
+
+eleve1 = Eleve("Mohammed", 17, 170)
+eleve2 = Eleve("Said", 16, 160)
+eleve2 = Eleve("Djelloul", 15)
